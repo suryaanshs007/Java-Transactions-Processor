ace0fb88722ff330ce2815522eaee0fedcb21907
suryaanshs007 <suryaanshs0@gmail.com>
2026-07-12 00:00:26 +0530

    Redesign filtering System with custom Predicates
    This one's a full filtering system redesign. Previously, I had methods with
    only a single filtering condition, just to see if my pipeline worked. Now, I have a full
    filter system redesign, it involves a single filter predicate of type CdmPipeLineBuild.Data, so
    I can practically use the filter in any possible way, instead of defining a million functions for
    each filter type. An if-else ladder handles the instantiation of the filter itself, the user has to
    enter a filter type and the filtering condition value, based on this, the display function simply calls
    the defineFilter function to assign a filter and the predicate gets used with the .filter stream function and
    a standard print operation using CdmPipeLineBuild.Data's display method is called by the consumer within the
    final .forEach in the filtered stream. im sure this could be made more advanced and less verbose, but this
    is the best i could think of for now, like i said, it'll get better with time.

--- src/CdmPipeLineProcessor.java
+++ src/CdmPipeLineProcessor.java
@@ -9,27 +9,66 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class CdmPipeLineProcessor {
+    public Predicate<CdmPipelineBuild.Data> filter;
 
     private CdmPipelineBuild cdm;
-
     CdmPipeLineProcessor() {
         this.cdm = new CdmPipelineBuild();
     }
+    private void defineFilter(String t, String type){
+        if(type.equals("status")){ this.filter=(a)->{
+            return a.status.equals(t);
+        };
+        }
+        else if(type.equals("amount")){
+            this.filter = (a)->{
+                return a.amt==Double.parseDouble(t);
+            };
+        }
+        else if(type.equals("amount >=")){
+            this.filter=(a)->{
+                return a.amt>=Double.parseDouble(t);
+            };
+        }
+        else if(type.equals("amount <=")){
+            this.filter=(a)->{
+                return a.amt<=Double.parseDouble(t);
+            };
+        }
+        else if(type.equals("city")){
+            this.filter=(a)->{
+                return a.city.equals(t);
+            };
+        }
+        else if(type.equals("payment_method")){
+            this.filter=(a)->{
+                return a.paymentMeth.equals(t);
+            };
+        }
+        else if(type.equals("category")){
+            this.filter=(a)->{
+                return a.cat.equals(t);
+            };
+        }
+        else if(type.equals("customer_id")){
+            this.filter=(a)->{
+                return a.custID.equals(t);
+            };
+        }
+        else if(type.equals("transaction_id")){
+            this.filter=(a)->{
+                return a.transacId.equals(t);
+            };
+        }
 
-    public void displayContents() {
-        this.cdm.resultStream().forEach(a -> {
+        }
+    public void displayFilteredResults(String t, String type){
+        this.defineFilter(t, type);
+        this.cdm.resultStream().filter(this.filter).forEach((a)->{
             a.display();
         });
     }
 
-    public void displayGreatestTransacDetails() {
-        this.cdm
-            .resultStream()
-            .filter(a -> {
-                return a.amt >= 5000.0;
-            })
-            .forEach(a -> {
-                a.display();
-            });
+
+
     }
-}
--- src/CsvDataManagerMain.java
+++ src/CsvDataManagerMain.java
@@ -6,6 +6,13 @@ public class CsvDataManagerMain {
 
     public static void main(String[] args) {
         CdmPipeLineProcessor cdmp = new CdmPipeLineProcessor();
-        cdmp.displayGreatestTransacDetails();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter filtering category and it's corresponding filter value: ");
+        String cat = sc.next();
+        String val = sc.next();
+        try {cdmp.displayFilteredResults(val, cat);}
+        catch(NullPointerException e){
+            System.out.println("No results found. Also, check your input category syntax.");
+        }
     }
 }
